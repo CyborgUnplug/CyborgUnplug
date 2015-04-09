753d0b7efa12efc0e1a754f00253cd3fe872330c
Julian Oliver <JulianOliver@users.noreply.github.com>
2015-04-09 14:41:10 +0200

    Sanified the little build script. 	modified:   build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
-mkdir cyborg-unplug
+#!/bin/bash
+
+DIR=cyborg-unplug
+FW=cyborg-unplug-wr710n.bin
+
+if [ ! -f $DIR ]; then
+    mkdir $DIR
+elif [ -f $DIR/$FW ]; then
+    rm -f $DIR/$FW
+fi
 make image PACKAGES="aircrack-ng lighttpd wireless-tools iw php5 php5-cgi gnupg bash screen lighttpd-mod-cgi coreutils coreutils-base64" PROFILE=TLWR710 FILES="files"
-cp bin/ar71xx/openwrt-ar71xx-generic-tl-wr710n-v1-squashfs-sysupgrade.bin cyborg-unplug/cyborg-unplug-wr710n.bin
+cp bin/ar71xx/openwrt-ar71xx-generic-tl-wr710n-v1-squashfs-sysupgrade.bin $DIR/$FW
