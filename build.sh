b1b806e365e9fcfa131b14ee816975fa1e95588a
Julian Oliver <JulianOliver@users.noreply.github.com>
2015-04-18 02:52:46 +0200

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ if [ ! -f $DIR ]; then
 elif [ -f $DIR/$FW ]; then
     rm -f $DIR/$FW
 fi
-make image PACKAGES="aircrack-ng lighttpd wireless-tools iw php5 php5-cgi gnupg bash screen lighttpd-mod-cgi coreutils coreutils-base64" PROFILE=TLWR710 FILES="files"
+make image PACKAGES="horst aircrack-ng lighttpd wireless-tools iw php5 php5-cgi gnupg bash screen lighttpd-mod-cgi coreutils coreutils-base64" PROFILE=TLWR710 FILES="files"
 cp bin/ar71xx/openwrt-ar71xx-generic-tl-wr710n-v1-squashfs-sysupgrade.bin $DIR/$FW
