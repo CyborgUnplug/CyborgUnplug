212f6c3c5bd5f5175ef6b2f14d8ff373035d3d4e
Julian Oliver <JulianOliver@users.noreply.github.com>
2015-12-16 16:22:18 +0100

    modified:   files/www/cgi-bin/config.cgi 	modified:   files/www/configure.php 	modified:   files/www/share.php 	modified:   files/www/vpn.php

--- files/www/configure.php
+++ files/www/configure.php
@@ -1,11 +1,12 @@
 <?php include 'header.php';?>
 
 	<h1 id="headline">Configure Little Snipper</h1>
+    <br><br>
 	<div id="container_general">
 		<form method="get" id="start" action="cgi-bin/config.cgi">
-			<input name="access point" type="submit" value="ap" class='button'>
+			<input name="access point" type="submit" value="wifi" class='button'>
             <br><br>
-			<input name="updates" type="submit" value="update" class='button'>
+			<input name="updates" type="submit" value="updates" class='button'>
             <br><br>
 			<input name="alerts" type="submit" value="alerts" class='button'>
             <br><br>
