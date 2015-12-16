da51560fdb0d38b1a9b2bc2229da265734cb50a1
Julian Oliver <julian@rimu>
2015-12-16 18:55:04 +0100

    modified:   files/root/scripts/ping.sh

--- files/root/scripts/ping.sh
+++ files/root/scripts/ping.sh
@@ -19,7 +19,7 @@ readonly BINPATH=/usr/sbin/
 readonly CONFIG=/www/config
 readonly POLLTIME=15
 readonly ETH=eth0.2 # WAN interface
-readonly VPN=89.238.81.42
+readonly VPN= # remote server IP here
 
 while true;
 	do
