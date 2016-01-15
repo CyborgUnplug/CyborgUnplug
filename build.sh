6d49ebf97119f830ff5b662e39d4e127b11f4553
Julian Oliver <JulianOliver@users.noreply.github.com>
2016-01-15 19:27:03 +0100

    modified:   bin/cyborg-unplug-wr710n.bin 	modified:   build.sh

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ if [ ! -f $DIR ]; then
 elif [ -f $DIR/$FW ]; then
     rm -f $DIR/$FW
 fi
-make image PACKAGES="horst aircrack-ng lighttpd wireless-tools iw php5 php5-cgi php5-mod-curl libcurl curl gnupg bash screen lighttpd-mod-cgi coreutils coreutils-base64 openvpn-easy-rsa openvpn-openssl msmtp block-mount" PROFILE=TLWR710 FILES="files"
+make image PACKAGES="kmod-fs-vfat kmod-fs-hfs kmod-fs-ext4 horst aircrack-ng lighttpd wireless-tools iw php5 php5-cgi php5-mod-curl libcurl curl gnupg bash screen lighttpd-mod-cgi coreutils coreutils-base64 openvpn-easy-rsa openvpn-openssl msmtp block-mount" PROFILE=TLWR710 FILES="files"
 cp bin/ar71xx/openwrt-ar71xx-generic-tl-wr710n-v1-squashfs-sysupgrade.bin $DIR/$FW
