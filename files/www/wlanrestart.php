d0359b90cfa5b70ac6754e854e6a1652c5d5b1da
Julian Oliver <JulianOliver@users.noreply.github.com>
2016-01-17 20:21:13 +0100

    password length and channel <= 13 tests in wlan configuration UI

--- files/www/wlanrestart.php
+++ files/www/wlanrestart.php
@@ -1,8 +1,9 @@
 <?php include 'header.php';
 
     echo "<div class='warning'>";
-    echo "You have been disconnected<br>";
+    echo "You have been disconnected\n";
     echo "Reconnect with the new settings to continue.";
     echo "</div>";
+    echo "<br>";
     echo "<br>";
 include 'footer.php';?>
--- files/www/wlanset.php
+++ files/www/wlanset.php
@@ -1,5 +1,6 @@
 <?php include 'header.php';
 
+$gtg=1;
 if (! isset($_POST['ssid']) || ! isset($_POST['password']) || ! isset($_POST['channel'])) {
     echo "<div class='warning'>";
     echo "Nothing entered...nothing to do.\n";
@@ -22,36 +23,72 @@ if (! isset($_POST['ssid']) || ! isset($_POST['password']) || ! isset($_POST['ch
     }
     if(isset($_POST['password'])) {
         $pw = $_POST['password'];
-        $fn2 ='/tmp/config/wlanpw'; 
-        $f2 = fopen($fn2, 'w');
-        $ret = fwrite($f2, $pw);
-        fclose($f2); 
-        if($ret === false) {
-            die('There was an error writing this file');
-        } 
+        $min = 8;
+        $max = 63;
+        if (strlen($pw) > $max ) {
+            $gtg=0;
+            echo "<div class='warning'>";
+            echo "Password too long: must not be longer than 63 characters.\n";
+            echo "</div>";
+            echo "<form method='get' id='wlanconf' action='wlanconf.php'>";
+            echo "<input name='wlanconf' type='hidden' value='wlanconf'>";
+            echo "<input type='submit' value='try again' class='button'>";
+            echo "</form>";
+        } elseif (strlen($pw) < $min) {
+            $gtg=0;
+            echo "<div class='warning'>";
+            echo "Password too short: must be 8 characters or longer.\n";
+            echo "</div>";
+            echo "<form method='get' id='wlanconf' action='wlanconf.php'>";
+            echo "<input name='wlanconf' type='hidden' value='wlanconf'>";
+            echo "<input type='submit' value='try again' class='button'>";
+            echo "</form>";
+        } else {
+            $fn2 ='/tmp/config/wlanpw'; 
+            $f2 = fopen($fn2, 'w');
+            $ret = fwrite($f2, $pw);
+            fclose($f2); 
+            if($ret === false) {
+                die('There was an error writing this file');
+            } 
+        }
     }
     if(isset($_POST['channel'])) {
         $channel = $_POST['channel'];
-        $fn3 ='/www/config/channel'; 
-        $f3 = fopen($fn3, 'w');
-        $ret = fwrite($f3, $channel);
-        fclose($f3); 
-        if($ret === false) {
-            die('There was an error writing this file');
-        } 
+        $max = 13;
+        if ($channel > $max) {
+            $gtg=0;
+            echo "<div class='warning'>";
+            echo "Channels cannot be higher than 13. Please choose a lower number.\n";
+            echo "</div>";
+            echo "<form method='get' id='wlanconf' action='wlanconf.php'>";
+            echo "<input name='wlanconf' type='hidden' value='wlanconf'>";
+            echo "<input type='submit' value='try again' class='button'>";
+            echo "</form>";
+        } else {
+            $fn3 ='/www/config/channel'; 
+            $f3 = fopen($fn3, 'w');
+            $ret = fwrite($f3, $channel);
+            fclose($f3); 
+            if($ret === false) {
+                die('There was an error writing this file');
+            } 
+        }
     }
-    echo "<div class='warning warning3'>";
-    echo "WLAN data saved.\n";
-    //echo "$ret bytes written to auth file";
-    echo "</div>";
-    echo "<div class='warning warning3'>";
-    echo "Select reboot below to restart the WiFi.\n";
-    echo "You'll need to log in with the new settings.\n";
-    echo "</div>";
-    echo "<form method='get' id='wlanrestart' action='cgi-bin/config.cgi'>";
-    echo "<input name='wlanrestart' type='hidden' value='wlanrestart'>";
-    echo "<input type='submit' value='reboot wifi' class='button'>";
-    echo "</form>";
+    if ( $gtg == 1 ){
+        echo "<div class='warning warning3'>";
+        echo "WLAN data saved.\n";
+        //echo "$ret bytes written to auth file";
+        echo "</div>";
+        echo "<div class='warning warning3'>";
+        echo "Select reboot below to restart the WiFi.\n";
+        echo "You'll need to log in with the new settings.\n";
+        echo "</div>";
+        echo "<form method='get' id='wlanrestart' action='cgi-bin/config.cgi'>";
+        echo "<input name='wlanrestart' type='hidden' value='wlanrestart'>";
+        echo "<input type='submit' value='reboot wifi' class='button'>";
+        echo "</form>";
+    }
 }
 
 include 'footer.php';?>
