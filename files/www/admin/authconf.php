e0c50cd7775ad26a7750ad86c40c9f0ad85a68c3
Julian Oliver <julian@rimu>
2016-01-25 14:47:08 +0100

    modified:   www/active.php 	modified:   www/admin/alertsconf.php 	modified:   www/admin/authconf.php 	modified:   www/admin/authset.php 	modified:   www/admin/updateconf.php 	modified:   www/admin/updatenow.php 	modified:   www/admin/wlanconf.php 	modified:   www/admin/wlanrestart.php 	modified:   www/cgi-bin/config.cgi 	modified:   www/data/devices 	modified:   www/detect.php 	modified:   www/header-no-status.php 	modified:   www/share-visits.php 	modified:   www/share.php 	modified:   www/status.php 	modified:   www/style.css

--- files/www/admin/authconf.php
+++ files/www/admin/authconf.php
@@ -3,21 +3,21 @@
     <center>
     <h1 id="headline">Change admin password</h1>
     </center>
+    <br>
      <div id="container_general">
     <center>
-    Change the password used to protect the configuration menu
+    Set a new password for the configuration menu
     </center>
-    <br>
 	<div class='warning'>
-    For added security, ensure no one else is connected to Little Snipper during this step. 
-    If you forget your new password you'll have to either log into the device using the command line utility <i>ssh</i> or reset it, using the reset button. Visit <a href="https://plugunplug.net/guides/littlesnipper">https://plugunplug.net/guides/littlesnipper</a> for more.
+    For added security ensure no one else is connected to Little Snipper during this step. 
+    If you forget your new password you'll have to either log into the device
+using a command line utility or reset it. See <a href="https://plugunplug.net/guides">https://plugunplug.net/guides</a>
 	</div>
+        <br>
       <form enctype="multipart/form-data" action="authset.php" method="post">
-        Set new admin password 
         <input name="password" type="text" placeholder="plugunplug" />
         <br><br>
         <input type="submit" value="save" />
       </form>
-        <br><br>
       </div>
 <?php include '../footer.php';?>
--- files/www/admin/updateconf.php
+++ files/www/admin/updateconf.php
@@ -44,11 +44,12 @@
                 }
 			?>
             <br>
+            <center>
             By default Little Snipper checks for updates every day at
             midnight. The updates are applied on the next reboot. For your interest and/or peace-of-mind, each update directly
             corresponds to a revision in this <a href="https://github.com/JulianOliver/CyborgUnplug/tree/usa">publicly auditable
 code repository</a>.
-            <br>
+            </center>
     </div>
 
 <?php include '../footer.php';?>
