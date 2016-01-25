831aa73315d1f488da7cea40c0539e0253554e8a
Julian Oliver <JulianOliver@users.noreply.github.com>
2016-01-25 16:46:08 +0100

    modified:   www/style.css 	new file:   www/tos.php 	modified:   www/vpnchoose.php

---
+++ files/www/tos.php
@@ -0,0 +1,58 @@
+<?php include 'header.php';?>
+<br>
+<center>
+<h2 id="headline">ACCEPTABLE USE POLICY OF THE CYBORG UNPLUG VPN </h2>
+</center>
+<hr>
+Both the access to the Service and its usage are under the governance and
+protection of you, the owner of a Cyborg Unplug Device. Each Device has a unique
+certificate which is bound to the owner of that Device, here known as the User.
+<br><br>
+The User, taking due care, is responsible for all use of the Service through the
+Device, including any third party they give Service access to, via WiFi or
+Ethernet connection. As such, the User undertakes to prevent abuse of the Service or to refrain from using the Service
+her/himself for fraudulent or false activities or activities that are in
+contradiction to activities the character of which constitute a
+crime, an infraction or other illegal activities prohibited by law or other
+activities prohibited by the conditions expressed here. 
+<br><br>
+The User is fully responsible for all operations and activities conducted
+through the device and is obliged to comply with his national legal order and
+the national legal order of Dislocative UG, Germany, (here known as the
+Provider).
+<ol class="tos">
+<li>The User further undertakes to refrain from using the Service for any attack or
+attempted attacks (such as hacking, denial-of-service) against any other
+computer, Internet network or other technical devices, including software,
+servers, network appliances and vehicles that are connected to the Internet.</li>
+<li>Notuse the Service to transfer malware, such as computer viruses, trojans worms,
+etc.</li>
+<li>Not use any of the Services neither for accepting nor publishing nor
+providing harmful content, such as content in breach of copyright, pirated
+copies of copyrighted works (such as CDs, DVDs, software), child pornography,
+etc.</li>
+<li>Not use any of the Services for sending unsolicited commercial messages or
+other harmful or fraudulent content, data and information, such as SPAM emails,
+unsolicited SMS messages, advertising and marketing messages, etc.</li>
+<li>Not use anyof the Services for activities the content of which is child pornography,
+supporting or promoting criminal activities such as activities that include
+accepting, publishing, selling, linking, etc.</li>
+<li>Not use any of the Services for activities that constitute or could constitute
+an infraction, a crime or the preparation or attempt thereof that is punishable
+according to Provider’s national legal order, EU law, international law,
+including the User’s national legal order.</li>
+<li>Not use any of the Services to persecute, harass, threaten or intimidate a third party;
+not use any means that are able to endanger or curb the use of the Services by
+other users; not use the Service for excessive data transfer.</li>
+</ol>
+The Provider reserves the right to cancel and revoke the User’s VPN
+certificate if the User is found to be in breach of Acceptable Use.  Further,
+the Provider reserves the right to limit the User’s transfer speed if the daily
+average of data transferred by the User exceeds 1 GB/day.
+<br><br>
+Acceptable Usage Policy of the Cyborg Unplug VPN is an integral part of Terms of
+Service published on <a
+href="https://plugunplug.net/tos">https://plugunplug.net/tos</a>.
+<br><br>
+Valid from Dec 21, 2015
+<?php include 'footer.php';?>
