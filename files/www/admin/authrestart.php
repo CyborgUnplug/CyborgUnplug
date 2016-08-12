fe1fec04ad89f97a02c069d7cca0c6229948b2a5
Julian Oliver <julian@rimu>
2016-08-12 20:03:14 +0200

    deleted:    www/admin/authrestart.php 	modified:   etc/config/firewall 	modified:   etc/config/fstab 	modified:   etc/config/network 	modified:   etc/config/wireless 	modified:   etc/lighttpd/lighttpd.conf 	modified:   root/scripts/ping.sh 	modified:   root/scripts/start.sh 	modified:   root/scripts/sweep.sh 	modified:   root/scripts/update.sh 	modified:   root/scripts/vpn.sh 	modified:   root/scripts/wifi.sh 	modified:   www/admin/alertsconf.php 	modified:   www/admin/authconf.php 	modified:   www/admin/authset.php 	modified:   www/admin/cgi-bin/config.cgi 	modified:   www/admin/cgi-bin/environment 	modified:   www/admin/configure.php 	modified:   www/admin/detect.php 	modified:   www/admin/emailconf.php 	modified:   www/admin/finish.php 	modified:   www/admin/footer.php 	modified:   www/admin/header.php 	modified:   www/admin/index.php 	modified:   www/admin/index.php.conf 	modified:   www/admin/registered.php 	modified:   www/admin/status.php 	modified:   www/admin/style.css 	modified:   www/admin/territorial.php 	modified:   www/admin/updateconf.php 	modified:   www/admin/updatenow.php 	modified:   www/admin/vpn.php 	modified:   www/admin/vpnchoose.php 	modified:   www/admin/wlanconf.php 	modified:   www/admin/wlanrestart.php 	modified:   www/admin/wlanset.php

--- files/www/admin/authrestart.php
+++
@@ -1,8 +0,0 @@
-<?php include '../header.php';
-
-    echo "<div class='warning'>";
-    echo "Reconnect with the new settings to continue.";
-    echo "</div>";
-    echo "<br>";
-    echo "<br>";
-include '../footer.php';?>
