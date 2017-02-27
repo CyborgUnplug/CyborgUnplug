ac1b84655fb8ae767ede05e23431e78b269b63af
Julian Oliver <JulianOliver@users.noreply.github.com>
2017-02-27 13:34:52 +0100

    Added home page with login button. Fixed issue with WiFi ESSIDs containing whitespace
    	new file:   files/www/footer.php
    	new file:   files/www/header.php
    	new file:   files/www/img/logo.png
    	modified:   files/www/index.php
    	new file:   files/www/status.php
    	new file:   files/www/style.css
    	modified:   files/root/scripts/wifi.sh

---
+++ files/www/footer.php
@@ -0,0 +1,3 @@
+<center><footer><hr>
+model: World | id:  0017A5EFB512 | rev: fcc3f32
+</footer></center></div></body></html>
--- files/www/index.php
+++ files/www/index.php
@@ -1,4 +1,13 @@
+<?php include 'header.php';?>
+  <center>
+  <h1 id="headline">Welcome to Cyborg Unplug</h1>
+  </center>
 <?php
-
-  header('Location: '.'admin/');
-?>
+             echo "<div id='container_general'>";
+             echo "<center>";
+             echo "<form action='admin/index.php'>";
+             echo "<input name='detect' type='submit' value='log in' class='button'>";
+             echo "</form>";
+             echo "</center>";
+             echo "</div>";
+include 'footer.php';?>
