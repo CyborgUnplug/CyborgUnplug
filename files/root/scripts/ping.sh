208d79ec5ca199e71822c373c5f1cc4a9b790d22
Brennan Novak <hi@brennannovak.com>
2017-04-26 19:22:50 +0200

    made ping.sh more robust for status polling

--- files/root/scripts/ping.sh
+++ files/root/scripts/ping.sh
@@ -24,11 +24,11 @@ while true;
         # We call php-cgi here as /usr/bin/curl doesn't support SSL while PHP's
         # curl implementation does
         vpnstate=$(cat $CONFIG/vpnstatus)
-        if [[ $vpnstate == up ]]; then
-            echo "Waiting for VPN" > $CONFIG/networkstate
-        fi
         if [[ $vpnstate != start && $vpnstate != stop ]]; then
             loc=$(php-cgi /www/admin/curl.php | tr -d $'\r' | tail -n +3 | head -n 1)
+			if [[ $vpnstate == up ]]; then
+				echo "Waiting for VPN" > $CONFIG/networkstate
+			fi
             if [[ ! -z $loc ]]; then
                 echo online $loc > $CONFIG/networkstate
             else
