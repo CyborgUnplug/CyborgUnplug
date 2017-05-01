e3ca50fb12547f43da0e41fad62f8e9ab390cfcc
Brennan Novak <hi@brennannovak.com>
2017-05-01 14:11:28 +0200

    moved javascript to files in js/ dir refactored status bar a bit + other responseive fixes

--- files/www/admin/footer.php
+++ files/www/admin/footer.php
@@ -1,3 +1,28 @@
-<center><footer><hr>
-model: World | id:  0017A5EFB512 | rev: fcc3f32
-</footer></center></div></body></html>
+			<footer>
+				<ul>
+					<li><img src="/img/little-snipper-outline.svg" class="little-snipper"></li>
+					<li>Little Snipper (world)</li>
+					<li>ID: 0017A5EFB512</li>
+					<li>Version: 12345678</li>
+			</footer>
+		</div>
+	</body>
+	<script src="/admin/js/zepto-1.2.0.min.js"></script>
+	<script src="/admin/js/app.js"></script>
+	<script>
+	$(document).ready(function() {
+		var status_data = localStorage.getItem('status_data')
+		if (status_data) {
+			updateStatus(JSON.parse(status_data))
+		}
+
+		var auto_refresh = setInterval(function() {
+			$.getJSON('status.php', function(response) {
+				console.log(response)
+				localStorage.setItem('status_data', JSON.stringify(response))
+				updateStatus(response)
+			})
+		}, 5000)
+	})
+	</script>
+</html>
---
+++ files/www/admin/js/app.js
@@ -0,0 +1,97 @@
+// Cyborg Unplug - Little Snipper
+// Javascript used throughout the admin interface
+
+var updateStatus = function(response) {
+
+	var css_wifi    = 'wifi wifi-off wifi-error'
+	var icons_wifi  = 'icon-wifi icon-wifi-off icon-wifi-error'
+	var icons_vpn   = 'icon-vpn-connecting icon-vpn-error icon-vpn-tunneled'
+	var colors      = 'waiting connecting connected tunneled disconnected'
+	var status_icon = "icon-connection-error"
+	var flag_img    = '<img src="/img/blank.png" class="flag flag-ISO">'
+
+	// Wifi & other
+	if (response.ssid == "unavailable") {
+		var wifi_css  = "wifi-error"
+		var wifi_icon = "icon-wifi-error"
+		var wifi_text = "Wifi Unavailable"
+	} else {
+		var wifi_css  = "wifi"
+		var wifi_icon = "icon-wifi"					
+		var wifi_text = response.ssid
+	}
+
+	$wifi = $('#your-wifi')
+	$wifi.removeClass(css_wifi).add(wifi_css)
+	$wifi.find('i').removeClass(icons_wifi).addClass(wifi_icon)
+	$wifi.find('span').html(wifi_text)
+
+	// Connection Status
+	if (response.status == "tunneled" && response.vpn == "up") {
+		var status_icon = "icon-vpn-tunneled"
+	} else if (response.status == "connecting" && response.vpn == "start") {
+		var status_icon = "icon-vpn-connecting"
+	} else if (response.status == "connecting") {
+		var status_icon = "icon-connecting"
+	} else if (response.status == "connected") {
+		var status_icon = "icon-connected"
+	} else if (response.status == "disconnected") {
+		var status_icon = "icon-disconnected"
+	}
+
+	$('#status_color')
+		.removeClass(colors)
+		.addClass(response.status)
+		.html('<i class="'+ status_icon + '"></i>')
+	$('#status_message').html(response.message)
+
+	// Connection Route
+	if (response.ip_country == 'Unspecified') {
+		$('#status-type').css('text-align', 'center')	
+		$('#status-route').hide()
+	} else {
+		var flag = flag_img.replace('ISO', response.ip_iso.toLowerCase())
+		var route = 'Via ' + response.ip_country
+
+		$('#status-route').find('span.flag').html(flag)
+		$('#status-route').find('span.country').html(route)
+		$('#status-route').find('span.ip').html(response.ip)
+		$('#status-route').show()
+	}
+}
+
+// Wifi Bridge (show / hide password field)
+function checkForPw() {
+	var myselect = $("#bridge").val();
+	if(myselect.split(",")[3] != 'off') {
+		$("#bridgepw").css('visibility', 'visible')
+	} else {
+		$("#bridgepw").css('visibility', 'hidden');
+	}
+}
+
+var $checkboxes
+function chooser() {         
+	var choices = $checkboxes.map(function() {
+		if (this.checked) {
+			return this.id
+		}
+	}).get().join(' ')
+	document.getElementById("checkList").value = choices
+}
+
+$(function() {
+	$checkboxes = $('input[type=checkbox]').change(chooser)
+
+	$('.navLink').on('click', function(e){
+		e.preventDefault()
+		var targetDiv = $($(this).attr('href'))
+		if (targetDiv.css('visibility') == 'visible'){
+			$('.page').css('display', 'none')
+			targetDiv.css('display', 'inline')
+		}
+		else {
+			$('.page').css('display', 'none')
+		}
+	})
+})
