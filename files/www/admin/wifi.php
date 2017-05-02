a2e0e088939a54615a4ac92e806df920b07296f1
julian <julian@rimu2>
2017-05-02 02:25:37 +0200

    Seemed to have reduced physical memory consumption by php-cgi processes by setting "PHP_FCGI_CHILDREN" => "1".
    Increased 'memory_limit' from 2M to 8M in /etc/php.ini, alongside an equal value
    for 'post_max_size'. This should help reading and displaying contents on USB
    sticks without hitting a wall.
    
    	modified:   files/etc/lighttpd/conf.d/fastcgi.conf
    	modified:   files/etc/php.ini
    	modified:   files/www/admin/wifi.php

--- files/www/admin/wifi.php
+++ files/www/admin/wifi.php
@@ -2,12 +2,8 @@
 $page_title = "Wifi";
 include('header.php'); ?>
 
-More detailed information about Wifi hotspot will go here...
-
-Ability to turn it on / off
-
 <form action="wlanconf.php">
-	<input name="wifi" type="submit" value="wifi hotspot setup" class="button">
+	<input name="wifi" type="submit" value="WiFi hotspot setup" class="button">
 </form>
 
 <?php include('footer.php'); ?>
