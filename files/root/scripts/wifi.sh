38d8aefbba8397ecce326a904780c58419f47711
julian <JulianOliver@users.noreply.github.com>
2017-05-15 03:45:27 +0200

    modified:   files/root/scripts/wifi.sh

--- files/root/scripts/wifi.sh
+++ files/root/scripts/wifi.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+#
+# Cyborg Unplug Wi-Fi management script for RT5350f LittleSnipper. 
+# 
+# Copyright (C) 2015 Julian Oliver 
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 readonly SCRIPTS=/root/scripts
 readonly CONFIG=/www/config
